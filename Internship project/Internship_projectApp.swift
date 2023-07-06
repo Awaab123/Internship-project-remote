c4cff8101ead3bd0a5752d15df80ce68fffbd645
Loai AhmedBabikir <saco@sacobayarea.org>
2023-07-06 00:59:50 -0700

    Initial Commit

---
+++ Internship project/Internship_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Internship_projectApp.swift
+//  Internship project
+//
+//  Created by Awaab Mirghani on 7/6/23.
+//
+
+import SwiftUI
+
+@main
+struct Internship_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
